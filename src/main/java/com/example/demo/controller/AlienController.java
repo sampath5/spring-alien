ee3a4b90da782d95f30b9490eee53bb434ed99e2
sampath kumar <sampath5898ch@gmail.com>
2020-05-07 12:46:56 +0530

    First

---
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.dao.AlienRepo;
+import com.example.demo.model.Alien;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import com.sun.org.slf4j.internal.LoggerFactory;
+//
+//import ch.qos.logback.classic.Logger;
+
+@Controller
+public class AlienController {
+	
+	@Autowired
+	AlienRepo repo;
+	
+	private static final Logger logger = LoggerFactory.getLogger(AlienController.class);
+
+	@RequestMapping("/")
+	public String Home() {
+		return "Show";
+	}
+	
+	@RequestMapping("/addAlien")
+	@ResponseBody
+	public String addAlien(Alien alien) {
+		System.out.println(alien);
+		logger.info("1st log message");
+		repo.save(alien);
+		return "Successful";
+	}
+	
+	@RequestMapping("/getAlien")
+	@ResponseBody
+	public Alien getAlien(@RequestParam int aid) {
+		//ModelAndView mv = new ModelAndView("showData");
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		//mv.addObject(alien);
+		return alien;
+	}
+	
+	@RequestMapping("/getByTech")
+	@ResponseBody
+	public List<Alien> getByTech(@RequestParam String tech) {
+		System.out.println(repo.getByTech(tech));
+		return repo.getByTech(tech);
+	}
+	
+	@RequestMapping("/findByTech")
+	@ResponseBody
+	public List<Alien> Test(@RequestParam String tech) {
+		System.out.println(repo.findByTech(tech));
+		return repo.findByTech(tech);
+	}
+
+}
